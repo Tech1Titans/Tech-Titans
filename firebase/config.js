1f78bc02bb0c2dfe286417e1aa2db650c50a1c7d
MahmoudAlla88 <hamoodaalla88@gmail.com>
2025-03-17 11:02:05 +0300

    structure

---
+++ firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_2ZRI9mgUuFW5JorssdG1qOwChtXCcBQ",
+  authDomain: "hackathon-279db.firebaseapp.com",
+  databaseURL: "https://hackathon-279db-default-rtdb.firebaseio.com",
+  projectId: "hackathon-279db",
+  storageBucket: "hackathon-279db.firebasestorage.app",
+  messagingSenderId: "306789052512",
+  appId: "1:306789052512:web:564f2562a59b78e6dcd305"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
